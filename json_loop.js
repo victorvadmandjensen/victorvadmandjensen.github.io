b88a7b9e255ec689ebe6508155fe4754ae9932d4
vict1311 <villerdk@live.com>
2025-06-04 22:05:24 +0200

    json

---
+++ json_loop.js
@@ -0,0 +1,50 @@
+/*
+// below is an example of how the papers should be structured
+<p class="mb-1 text-start"><b>Jensen, V. V.</b>, Christensen, E., Christiansen, I., Pham, K., Hansen, N. B., & Persson, J. S. 
+    2025. 
+    Designing for Digital Mediation of the Volatile Electricity Prices in the European Energy Crisis.
+    In <i>Scandinavian Journal of Information Systems</i>.
+    <!-- DOI: 10.1007/s10257-025-00704-7. -->
+    <p class="text-start">
+        <a href = 'https://aisel.aisnet.org/sjis_preprints/1/' target="_blank"> Preprint </a>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </p>
+</p> 
+
+// and a paper might look like this data structure
+    "authors": "Jensen, V. V., Christensen, E., Christiansen, I., Pham, K., Hansen, N. B., & Persson, J. S.",
+    "year": "2025. ",
+    "title": "Designing for Digital Mediation of the Volatile Electricity Prices in the European Energy Crisis.",
+    "venue": "Scandinavian Journal of Information Systems",
+    "link": "https://aisel.aisnet.org/sjis_preprints/1/"
+
+
+*/ 
+
+// fetch the JSON with GET, resolve the response by returning the JSON in the response, then get the JSON
+const json_data = fetch("https://victorvadmandjensen.github.io/academic_papers.json", {
+                    method: "GET"
+                    })
+                    .then(function(response) {
+                        return response.json(); 
+                    })
+                    .then(function(json) {
+                        display_papers(json);
+                    });
+
+function display_papers(papers) {
+    const container = document.getElementById("paper_container");
+
+    papers.array.forEach(element => {
+        const paperElement = document.createElement("p");
+        paperElement.className("mb-1 text-start");
+
+        paperElement.innerHTML = `
+        $element.authors
+        `;
+
+        container.appendChild(paperElement);
+    });
+}
+
+//display_papers(json_data);
